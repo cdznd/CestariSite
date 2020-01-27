890c4b75d684af8d14c0c4ec6af35f38902fe432
unknown <floyd9732@gmail.com>
2020-01-27 11:36:15 -0300

    Alteração nos cursos e mais duas abas na navbar

---
+++ assets/js/navscript.js
@@ -0,0 +1,15 @@
+$(window).scroll(function(){
+
+    if($(document).scrollTop() > 300){
+
+        $('nav').addClass('shrink');
+
+    }else{
+
+        $('nav').removeClass('shrink');
+
+    }
+
+
+});
+
